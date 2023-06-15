91aec2af5c6c477e86210771ca8ad1e2fe384bfb
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-15 08:14:27 -0700

    error fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Prints a string using puts()
  * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like builiding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
