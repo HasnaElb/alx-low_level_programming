994a4b732136a496c0f6f905c6787378849a8740
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-16 05:03:14 -0700

    print all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char abc;
+
+	for (num = 0; num <= 9; num++)
+	{
+	putchar(48 + num);
+	}
+	for (abc = 'a'; abc <= 'f'; abc++)
+	{
+	pytchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}
+
