8b0c321f6dac8eae15d27b538afc1815bd52d195
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-16 11:43:49 -0700

    print all possible different combinaisons of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numI;
+	int numJ;
+	int minI = 48;
+
+	for (numI = 48; numI <= 57; numI++)
+	{
+		for (numJ = minI++; numJ <= 57; numJ++)
+		{
+		if (numJ != numI)
+		}
+		putchar(numI);
+		putchar(numJ);
+		if (!(numJ == 57 && numI == 56))
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
