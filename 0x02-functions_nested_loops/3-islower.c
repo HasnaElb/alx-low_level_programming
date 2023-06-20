cbef47a387b4efb3a55ac1b51a9509de24bb34ea
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-20 09:37:53 -0700

    check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: contain a value to be compared
+ * Return: on success, 1 otherwise 0
+ */
+int _islower(int c)
+{
+	for (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+return (0);
+}
