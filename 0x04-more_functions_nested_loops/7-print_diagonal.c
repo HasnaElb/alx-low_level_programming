7fb355a2a649576240bc83cff655620dfb1003f7
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-22 09:46:36 -0700

    errors fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,7 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n <= 0)
 	{
@@ -16,7 +17,11 @@ void print_diagonal(int n)
 	}
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\\');
+	for (j = 0; j < i; j++)
+	{
+		_putchar(' ');
 	}
+	_putchar('\\');
 	_putchar('\n');
+	}
 }
