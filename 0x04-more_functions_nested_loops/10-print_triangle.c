56ea69375f4a6518e03b5c2f5a89a401e6fd978f
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-22 14:19:22 -0700

    error fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,22 @@ void print_triangle(int size)
 		_putchar('\n');
 		return;
 	}
+
 	for (i = 1; i < (size + 1); i++)
 	{
+
+
 		for (j = 0; j < (size - 1); j++)
 		{
 			_putchar(' ');
 		}
+
+
 		for (k = 0; k < i; k++)
 		{
 			_putchar('#');
 		}
+
 		_putchar('\n');
 	}
 }
