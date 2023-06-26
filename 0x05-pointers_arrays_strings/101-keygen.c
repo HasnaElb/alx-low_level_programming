3fdca8a770a557c9b4e49773e89d2142ef0a0811
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-26 11:55:26 -0700

    function that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function that generates random valid passwords for the
+ * program 101-crackme
+ * Return: Always 0
+ */
+int main(void)
+{
+	char keygen[84];
+	int index = 0, sum = 0;
+	int diff_half1, diff_half2;
+
+	srand(time(0));
+	while (sum < 2772)
+	{
+		keygen[index] = 33 + rand() % 94;
+		sum += keygen[index++];
+	}
+	keygen[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+		for (index = 0; keygen[index]; index++)
+		{
+			if (keygen[index] >= (33 + diff_half1))
+			{
+				keygen[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; keygen[index]; index++)
+		{
+			if (keygen[index] >= (33 + diff_half2))
+			{
+				keygen[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", keygen);
+
+	return (0);
+}
