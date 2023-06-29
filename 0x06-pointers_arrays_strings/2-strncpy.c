ce787dbcbf0217deba4d0a6a0220035d593dfbd7
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-29 15:59:59 -0700

    error fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,24 +8,26 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int i = 0, len = 0;
 
-	while (*src)
+	while (src[len] != '\0')
+		len++;
+
+	while (i < n && src[i] != '\0')
 	{
-		srclen++;
-		src++;
+		dest[i] = src[i];
+		i++;
+	}
+	len++;
+
+	if (n > len)
+	{
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
 	}
 
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	return (dest);
 }
