a5b0f88d29c667bf2e7e5f7ab44b0efcc698663f
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-06-30 05:08:03 -0700

    function that encodes a string using rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string
+ * @s: an input string to encode
+ * Return: an encode string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (s);
+}
