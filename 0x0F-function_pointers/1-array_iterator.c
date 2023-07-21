2a12242b705ba6ff58892b01eb272822699ec4fb
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-07-21 07:24:08 -0700

    error fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	i = 0;
 
-	if (array != NULL || action != NULL)
+	if (action != NULL && array != NULL)
 	{
-	for (i = 0; i < size; i++)
-		action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 	}
 }
