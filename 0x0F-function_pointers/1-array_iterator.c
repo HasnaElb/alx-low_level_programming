b82862a75a83f80bea44979161586f10d6a8a39e
Hasna Elboukhari <hasna.elboukhari.neuro@gmail.com>
2023-07-21 07:26:13 -0700

    error fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * array_iterator - Executes a function given as a parameter on each
  * element of an array
