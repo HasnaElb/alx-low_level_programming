b32ebdd5f0d8e6cc4a18b362b4bd8540d2109778
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-11 09:44:00 -0800

    file corrected

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/HasnaElb/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
+wget -p https://github.com/HasnaElb/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
