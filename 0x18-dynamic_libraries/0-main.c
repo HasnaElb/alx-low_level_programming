7c431b4e3962070aab76a2cc972f28c19647ef9b
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-11 08:37:05 -0800

    file created

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
